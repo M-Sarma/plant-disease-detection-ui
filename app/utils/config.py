5a6b89352a7e8ec8139186ae71b39e98a7310e66
M-Sarma <mithunsarma@gmail.com>
2025-01-07 21:52:57 +0530

    Update config.py

--- app/utils/config.py
+++ app/utils/config.py
@@ -1,4 +1,4 @@
 class AppConfig:
-    PREDICTION_ENDPOINT = " http://127.0.0.1:5000/api/predict"
-    FEEDBACK_ENDPOINT = " http://127.0.0.1:5000/api/feedback"
-    HEATMAP_ENDPOINT = " http://127.0.0.1:5000/api/heatmap"
+    PREDICTION_ENDPOINT = " http://16.171.139.158:5000/api/predict"
+    FEEDBACK_ENDPOINT = " http://16.171.139.158:5000/api/feedback"
+    HEATMAP_ENDPOINT = " http://16.171.139.158:5000/api/heatmap"
